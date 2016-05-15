befb960c091ab165665258e04d4e041984a4c435
Eder Eduardo <eder.silva@luizalabs.com>
2016-05-15 17:52:18 -0300

    fixed routes now server send static files and index routes

--- generators/app/templates/express/server.js
+++ generators/app/templates/express/server.js
@@ -3,8 +3,10 @@ var express = require('express');
 var app = express();
 var glob = require('glob');
 var fs = require('fs');
+var path = require('path');
 var PORT = 4000;
 var content;
+app.use(express.static(path.join(__dirname, 'apps')));
 
 glob("./routes/*.json", function (er, files) {
   for(var i=0; i < files.length; i++) {
@@ -12,13 +14,14 @@ glob("./routes/*.json", function (er, files) {
     for(var k=0; k < content.app.routes.length; k++){
       console.log(content.app.routes[k])
       app.get(content.app.routes[k], function (req, res) {
-        res.send('route name ' + content.app.routes[k]);
+        res.render('index.html');
       });
     }
   }
 });
 
-app.use('/apps', express.static(__dirname + '/public'));
+
+app.use('/public', express.static(__dirname + '/apps'));
 
 app.listen(PORT, function () {
   debug('Wall-e server listening on port ' + PORT);
